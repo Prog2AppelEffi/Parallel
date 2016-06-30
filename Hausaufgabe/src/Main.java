aa5845a04b972af93c46af1b0cf3519392c58a7c
Martin Appelmann <m.appelmann@tu-bs.de>
2016-06-30 13:57:39 +0200

    hausaufgabe fertig / sorry :D

--- Hausaufgabe/src/Main.java
+++ Hausaufgabe/src/Main.java
@@ -9,40 +9,71 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 11; i++) {
-            liste.add(i);
-        }
-        /*
+
         for (int i = 0; i < 50; i++){
             int randomNumber = (int) (Math.random()*100) + 1;
             if (!liste.contains(randomNumber)){
                 liste.add(randomNumber);
             }
-        }*/
+        }/**/
+
+        //=========================================erster thread========================================================
 
         Runnable deleteRunnable = new Runnable() {
             @Override
             public void run() {
+                System.out.println("-------------------------------------");
+                System.out.println("delete thread gestartet");
+                System.out.println("-------------------------------------");
                 for (int i = 0; i < liste.size(); i++){
-                    syncDelete(i);
+                    synchronized (Main.class) {
+                        System.out.println("thread delete will auf " + liste.get(i) + " mit dem index " + i + " zugreifen.");
+                        if (!checkPrim(liste.get(i))) {
+                            System.out.println(liste.get(i) + " gelöscht");
+                            liste.remove(i);
+                            i--;
+                            System.out.println(liste.toString());
+                        }
+                    }
                 }
+                System.out.println("-------------------------------------");
+                System.out.println("delete thread beendet");
+                System.out.println("-------------------------------------");
             }
         };
+
+        //============================================zweiter thread====================================================
 
         Runnable insertRunnable = new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 10; i++) {
-                    syncInsert(i);
+                System.out.println("-------------------------------------");
+                System.out.println("insert thread gestartet");
+                System.out.println("-------------------------------------");
+                for (int i = 1; i < 100; i++) {
+                    synchronized (Main.class) {
+                        System.out.println("thread insert will die Zahl " + i + " hinzufügen");
+                        if (checkPrim(i)) {
+                            if (!liste.contains(i)) {
+                                System.out.println((i) + " hinzugefügt");
+                                liste.add(i);
+                                System.out.println(liste.toString());
+                            }
+                        }
+                    }
                 }
+                System.out.println("-------------------------------------");
+                System.out.println("insert thread beendet");
+                System.out.println("-------------------------------------");
             }
         };
 
         Thread deleteThread = new Thread(deleteRunnable);
         Thread insertThread = new Thread(insertRunnable);
 
-
+        System.out.println("================================anfangsliste==========================================");
         System.out.println(liste.toString());
+        System.out.println("======================================================================================");
 
         deleteThread.start();
         insertThread.start();
@@ -53,35 +84,17 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+        System.out.println("==================================endliste===========================================");
         System.out.println(liste.toString());
+        System.out.println("=====================================================================================");
     }
 
-    private synchronized static void syncDelete(int i){
-        //System.out.println(i + " index " + liste.get(i) + " zahl");
-        System.out.println("thread delete number " + liste.get(i) + " index: " + i);
-        if (!checkPrim(liste.get(i))){
-            System.out.println(liste.get(i) + " gelöscht mit dem index " + i);
-            liste.remove(i);
-            i--;
-        }
-    }
-
-    private synchronized static void syncInsert(int i){
-        System.out.println("thread insert number " + i + " index: " + (i+1));
-        if (checkPrim(i + 1)){
-            if (!liste.contains(i + 1)){
-                liste.add(i + 1);
-            }
-        }
-    }
 
     public static boolean checkPrim(int number){
         if (number == 1){
             return false;
         }
         for (int i = 2; i < number; i++) {
-            //System.out.println(number + " % " + i + " " + (number%i));
             if (number % i == 0){
                 return false;
             }
